76f4ffaab77257241b5b1a315758e9bd00e72a74
Arifur999 <arr96777@gmail.com>
2025-11-25 23:25:38 +0600

    added post api

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,3 +2,21 @@ import dotenv from "dotenv"
 import path from "path"
 
 dotenv.config({path:path.join(process.cwd(),".env")})
+
+
+const config = {
+  env: process.env.NODE_ENV || "development",         // keep as string
+  port: Number(process.env.PORT) || 5000,             // convert to number + default
+};
+
+
+
+
+// const config ={
+// env : process.env.NODE_ENV ? Number(process.env.NODE_ENV) : 5000,
+// port: process.env.PORT,
+
+
+
+// }
+export default config
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,59 @@
-import http, {IncomingMessage, Server, ServerResponse} from "http"
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+import config from "./config";
 
-const server : Server=http.createServer((req:IncomingMessage,res:ServerResponse)=>{
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("start server......");
 
-console.log("start server......");
-if (req.url=='/' && req.method== 'GET') {
-    res.writeHead(200,{"content-type":"application/json"})
-    res.end(JSON.stringify(
-        {
-            message:"hello from node js with typescript....",
-            path:req.url,
-        }
-    ));
+//root rout
+
+    if (req.url == "/" && req.method == "GET") {
+      res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "hello from node js with typescript....",
+          path: req.url,
+        })
+      );
+    }
+
+
+
+
+if (req.url=='/api' && req.method=="GET") {
+       res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify({
+          message: "typescript boom boom....",
+          path: req.url,
+        })
+      );
 
 
 }
 
 
-})
+if (req.url=='/api/user' && req.method=="POST") {
+    const user ={
+        id:1,
+        name:"alice"
+    };
 
 
 
-server.listen(5000,()=>{
-    console.log(`server is running ${5000}`);
+           res.writeHead(200, { "content-type": "application/json" });
+      res.end(
+        JSON.stringify(user)
+      );
+
+}
+
+
+
+
+  }
+);
+
+server.listen(config.port, () => {
+  console.log(`server is running ${config.port}`);
 });
-
-
