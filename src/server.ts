4b9f59d51af792af764665c759ec8354d4729647
Arifur999 <arr96777@gmail.com>
2025-11-27 21:41:10 +0600

    added row routes and POST api and create json and dynamic id

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,41 @@ import config from "./config";
 import { json } from "stream/consumers";
 import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes/routes";
+import { match } from "assert";
+
+
+
+function findDynamicRoute(method:string,url:string){
+    const methodMap =routes.get(method);
+    if (!methodMap) return null
+
+    for(const [routePath,handler]of methodMap.entries()){
+        const routeParts=routePath.split('/');
+        const urlParts=url.split('/')
+
+
+        if (routeParts.length !== urlParts.length) continue;
+        const params:any={};
+        let matched=true;
+
+        for (let i = 0; i < routeParts.length; i++) {
+            if (routeParts[i]?.startsWith(':')) {
+                params[routeParts[i]?.substring(1)!]=urlParts[i]
+            }else if(routeParts[i]!== urlParts[i]){
+                matched=false;
+                break;
+
+            }
+            
+        }
+
+        if (matched) {
+            return {handler,params};
+        }
+    }
+    return null;
+}
+
 
 
 
@@ -19,7 +54,14 @@ const server: Server = http.createServer(
 
 if (handler) {
     handler(req,res);
-}else{
+}
+else if  (findDynamicRoute(method,path)){
+const match =findDynamicRoute(method,path);
+(req as any).params=match?.params;
+match?.handler(req,res);
+}
+
+else{
     res.writeHead(404,{"content-type":"application/json"});
     res.end(
         JSON.stringify({
