5345601b9dc506ae017940f422677ab6761667cb
Arifur999 <arr96777@gmail.com>
2025-11-27 22:05:53 +0600

    added row routes and PUT api and dynamic id

--- src/server.ts
+++ src/server.ts
@@ -4,40 +4,7 @@ import { json } from "stream/consumers";
 import addRoutes, { RouteHandler, routes } from "./helpers/RouteHandler";
 import "./routes/routes";
 import { match } from "assert";
-
-
-
-function findDynamicRoute(method:string,url:string){
-    const methodMap =routes.get(method);
-    if (!methodMap) return null
-
-    for(const [routePath,handler]of methodMap.entries()){
-        const routeParts=routePath.split('/');
-        const urlParts=url.split('/')
-
-
-        if (routeParts.length !== urlParts.length) continue;
-        const params:any={};
-        let matched=true;
-
-        for (let i = 0; i < routeParts.length; i++) {
-            if (routeParts[i]?.startsWith(':')) {
-                params[routeParts[i]?.substring(1)!]=urlParts[i]
-            }else if(routeParts[i]!== urlParts[i]){
-                matched=false;
-                break;
-
-            }
-            
-        }
-
-        if (matched) {
-            return {handler,params};
-        }
-    }
-    return null;
-}
-
+import findDynamicRoute from "./helpers/dynamicRoutes";
 
 
 
